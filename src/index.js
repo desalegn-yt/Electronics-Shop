ff40cebcef956ce4843cb99c59b554e13887dfba
Desalegn Yismaw <DesalegnYi@ethiopianairlines.com>
2024-04-06 16:32:35 +0300

    login implemented with auth0

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { Auth0Provider } from '@auth0/auth0-react';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+<Auth0Provider
+    domain="dev-4g7ek1a2zxwq5svm.us.auth0.com"
+    clientId="cuLz9TvLMW1KtdCrczTA1tLGAuPnNZvm"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
     <App />
-  </React.StrictMode>
+  </Auth0Provider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
